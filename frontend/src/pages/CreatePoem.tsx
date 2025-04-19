dc09ffe358e9004d321447f0d67d87d677a4e57f
AndyRosenberg <andynaterosenberg@gmail.com>
2025-04-19 15:46:49 -0700

    removing unused imports

--- frontend/src/pages/CreatePoem.tsx
+++ frontend/src/pages/CreatePoem.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useAuthRedirect } from '../hooks/useAuthRedirect';
 import { useCreatePoem } from '../hooks/useCreatePoem';
 import { UserAvatar } from '../components/UserAvatar';
